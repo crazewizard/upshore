f14a40b7a4ad1e0f66cd5a54a568b28dabb43fd8
crazewizard <crazewizard@gmail.com>
2025-10-23 20:02:47 +0100

    Optimised studio images

--- src/routes/Studio.tsx
+++ src/routes/Studio.tsx
@@ -3,7 +3,7 @@ import { PageHeader } from "~/components/PageHeader";
 
 export default function Studio () {
     const parentStyle = "flex flex-col w-full max-w-[3000px] justify-center mt-32 mx-auto";
-    const imageStyle= "max-h-[500px]";
+    const imageStyle= "max-h-[500px] rounded-2xl border border-tertiary";
 
     const images = import.meta.glob("~/studio/*.{png,jpg}", { eager:true, import:"default"}) as Record <string, string>;
     const imageList : string[] = Object.values(images);
