9284175c9d3e2e4d64b931c241a659b46aab256d
crazewizard <crazewizard@gmail.com>
2025-10-24 17:53:11 +0100

    Removed some conflicted code

--- src/components/Team.tsx
+++ src/components/Team.tsx
@@ -44,7 +44,7 @@ export function Avatar (props: { Person: Person; Style:DynamicStyle }) {
                         <p class="text-white/60">{props.Person.Title}</p>
 
                         <a href={`https://www.${props.Person.Social}`} class="flex p-4 gap-2" target="_blank" rel="noopener noreferrer" >
-                            <img src="/icons/LinkedIn.svg" class="w-[24px] h-[24px]" alt="LinkedIn icon"/>
+                            <img src="/icons/LinkedIn.svg" class="w-[24px] h-[24px]" alt="LinkedIn icon" />
                             <span class=" text-white">LinkedIn</span>
                         </a>
                     </div>
@@ -53,36 +53,3 @@ export function Avatar (props: { Person: Person; Style:DynamicStyle }) {
         </span>
     )
 }
-
-
-/*
-export default function TeamDetail (props: { Person : Person }) {
-    return (
-        <div    onMouseEnter={() => setShowFloating(true)}   // keep visible when hovering it
-                onMouseLeave={() => setShowFloating(false)}  // hide when leaving it
-                class={`pt-[80px] absolute top-[0px] left-0 transition-opacity duration-200
-                    ${
-                        showFloating() ? "opacity-100" : "opacity-0 pointer-events-none"
-                      }
-                    `}   
-                    >
-
-            <div class="flex flex-col gap-4 bg-black/90 backdrop-blur-sm
-                    w-[300px] md:w-[350px] h-[300px] md:h-[360px] rounded-4xl p-6 md:p-8">
-
-                <div class="absolute top-6 right-6 md:top-8 md:right-8 border border-white p-3 md:p-4 rounded-full">
-                    <img src="/public/icons/ArrowUPForward.svg" />
-                </div>
-
-                <img src={props.Person.Avatar} class="w-[120px] h-[120px]" />
-                <h3 class="text-white">{props.Person.Name}</h3>
-                <p class=" text-white">{props.Person.Title}</p>
-
-                <a href={props.Person.Social} class="flex p-4 gap-2" >
-                    <img src="/public/icons/LinkedIn.svg" class="w-[24px] h-[24px]" />
-                    <span class=" text-white">LinkedIn</span>
-                </a>
-            </div>
-        </div>
-    )
-}*/
