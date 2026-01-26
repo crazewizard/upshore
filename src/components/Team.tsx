46c9ab5a48bcae271658ec2a67e05a8ef9183fd1
crazewizard <crazewizard@gmail.com>
2026-01-26 21:32:36 +0100

    Fixed the foating modal for team. close onscroll

--- src/components/Team.tsx
+++ src/components/Team.tsx
@@ -1,46 +1,57 @@
 import { Person, Leonardo, Alison } from "../types/Person";
 import { DynamicStyle } from "~/types/DynamicStyle";
-import { Signal, createSignal } from "solid-js";
+import { Signal, createSignal, onCleanup, onMount } from "solid-js";
 import { A } from "@solidjs/router";
 
-export function Team (){
+export function Team() {
     return (
         <span class="flex -space-x-4">
-            <Avatar Person={Leonardo} Style={"w-16 h-16 bg-accent"}/>
-            <Avatar Person={Alison} Style={"w-16 h-16 bg-accent"}/>
+            <Avatar Person={Leonardo} Style={"w-16 h-16 bg-accent"} />
+            <Avatar Person={Alison} Style={"w-16 h-16 bg-accent"} />
         </span>
     );
 }
 
 const avatarStyle = "inline-block relative backdrop-blur-sm border border-border rounded-full bg-size-[100%] bg-cover bg-center bg-no-repeat";
 
-export function Avatar (props: { Person: Person; Style:DynamicStyle }) {
-    const [showFloating, setShowFloating] = createSignal (false);
+export function Avatar(props: { Person: Person; Style: DynamicStyle }) {
+    const [showFloating, setShowFloating] = createSignal(false);
+    onMount(() => {
+        const handleScroll = () => {
+            setShowFloating(false);
+        }
+        document.addEventListener("scroll", handleScroll);
+
+        onCleanup(() => {
+            document.removeEventListener("scroll", handleScroll);
+        })
+    })
     return (
-        <span   class={`flex ${props.Style} ${avatarStyle}`} 
-                style={{ "background-image": `url(${props.Person.Avatar})` }}
-                onMouseEnter={() => setShowFloating(true)}
-                onMouseLeave={() => setShowFloating(false)}
-                >
-            { showFloating() && (
-                <div    onClick = {()=> setShowFloating(true)}
-                        onMouseEnter={() => setShowFloating(true)}   // keep visible when hovering it
-                        onMouseLeave={() => setShowFloating(false)}  // hide when leaving it
-                        class={`pt-[76px] absolute top-0 left-0 
+        <span class={`flex ${props.Style} ${avatarStyle}`}
+            style={{ "background-image": `url(${props.Person.Avatar})` }}
+            onMouseEnter={() => setShowFloating(true)}
+            onMouseLeave={() => setShowFloating(false)}
+        >
+            {showFloating() && (
+                <div onClick={() => setShowFloating(true)}
+                    onMouseEnter={() => setShowFloating(true)}   // keep visible when hovering it
+                    onMouseLeave={() => setShowFloating(false)}  // hide when leaving it
+                    class={`pt-[76px] absolute top-0 left-0 
                                 ${showFloating() ? "block" : "hidden pointer-events-none "}
-                                `}   
-                        >
+                                `}
+                >
 
                     <div class="group relative flex flex-col gap-4 backdrop-blur-sm bg-overlay border border-border items-center
                                 w-[300px] md:w-[350px] h-auto rounded-4xl p-6 md:p-8 z-70 fade-down">
 
-                        <A  class="absolute top-4 right-4 md:top-6 md:right-6 border border-white p-2 md:p-3 rounded-full transition-opacity duration-500 opacity-40 hover:opacity-100"
-                            href={(props.Person === Leonardo) ? `https://www.${props.Person.Social}` : `/${props.Person.Name}` }
+                        <A class="absolute top-4 right-4 md:top-6 md:right-6 border border-white p-2 md:p-3 rounded-full transition-opacity duration-500 opacity-40 hover:opacity-100"
+                            href={(props.Person === Leonardo) ? `https://www.${props.Person.Social}` : `/${props.Person.Name}`}
+
                             /*target="_blank" rel="noopener noreferrer"*/ >
-                            <img src="/icons/ArrowUPForward.svg" class="w-6 h-6" alt="Open link"/>
+                            <img src="/icons/ArrowUPForward.svg" class="w-6 h-6" alt="Open link" />
                         </A>
 
-                        <img src={props.Person.Avatar} class="w-[120px] h-[120px]" alt="Team member"/>
+                        <img src={props.Person.Avatar} class="w-[120px] h-[120px]" alt="Team member" />
                         <h3 class="text-white">{props.Person.Name}</h3>
                         <p class="text-white/60">{props.Person.Title}</p>
 
@@ -50,7 +61,7 @@ export function Avatar (props: { Person: Person; Style:DynamicStyle }) {
                         </a>
                     </div>
                 </div>
-                    )}
+            )}
         </span>
     )
 }
